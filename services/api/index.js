904dd76e343e6ab85d05c1386cdc8c0d010f33ba
Udit Rai <uditrai88@Udits-MacBook-Air.local>
2025-05-07 20:30:09 +0530

    Added logging capabilities

--- services/api/index.js
+++ services/api/index.js
@@ -1,1 +1,14 @@
-console.log('CRM Summarizer API running');
+const logger = require('./logger');
+const express = require('express');
+const app = express();
+
+logger.info('CRM Summarizer API starting up...');
+
+app.get('/', (req, res) => {
+  logger.info('Received request on /');
+  res.send('Hello from CRM Summarizer!');
+});
+
+app.listen(3000, () => {
+  logger.info('Server running on port 3000');
+});
