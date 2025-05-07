f7a06368751fb7ad0171c1c4043526791472b457
Udit Rai <uditrai88@Udits-MacBook-Air.local>
2025-05-07 12:59:28 +0530

    removed the infra folder only

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "ap-south-1" # or your preferred region
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "crmsummarizer-demo-bucket-udit88"
-  acl    = "private"
-}
